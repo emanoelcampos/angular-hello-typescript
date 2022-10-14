20da3b07cda4399f9c3bc79ce3fc3da7c6955d14
emanoelcampos <emanoel.cv@gmail.com>
2022-10-13 21:46:09 -0300

    hello typescript

---
+++ main.ts
@@ -0,0 +1,25 @@
+var minhaVar = 'minha variavel';
+
+function minhaFuncao(x: number, y:number) {
+    return x + y;
+}
+
+//ES 6 ou ES 2015
+let num2 = 2;
+const PI2 = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2)
+
+class Matematica2 {
+    soma(x:number, y:number) {
+        return x + y;
+    }
+}
+
+//explicação de tipagem de variáveis
+var nome2 = 'emanoel';
